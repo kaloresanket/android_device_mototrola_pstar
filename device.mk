339c799841997664888300304a234bc98b635a36
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-11 01:05:37 +0200

    pstar: Move WCNSS_qcom_cfg.ini out of qca6390 subdir
    Change-Id: I6c162f6c232a5b8820bda954b681374c52e89bb8

--- device.mk
+++ device.mk
@@ -421,7 +421,7 @@ PRODUCT_PACKAGES += \
     init.vendor.st21nfc.rc \
     fstab.qcom \
     fstab.qcom.zramwb \
-    ueventd.qcom.rc \
+    ueventd.qcom.rc
 
 # RIL
 PRODUCT_PACKAGES += \
@@ -522,7 +522,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 # WiFi Display
 PRODUCT_PACKAGES += \
