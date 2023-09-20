5c2ed654721966928ee62fde6913fb6a4bfacc8a
kaloresanket <kaloresanket@gmail.com>
2023-09-20 17:37:52 +0530

    pstar : Build WFD libs

--- device.mk
+++ device.mk
@@ -457,5 +457,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
 
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libnl \
+    libwfdaac_vendor
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # Inherit vendor
 $(call inherit-product, vendor/motorola/pstar/pstar-vendor.mk)
