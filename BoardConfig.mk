8c4bee0ddb0f1dd4ee298f8def50a432e705d650
kaloresanket <kaloresanket@gmail.com>
2023-09-20 20:36:57 +0530

    pstar: Add power AIDL DT2W support via sysfs node

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,6 +125,9 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Platform
 TARGET_BOARD_PLATFORM := kona
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
