1d4cc65a34f952d94c07839a21b986d7c05b2b80
kaloresanket <kaloresanket@gmail.com>
2023-09-20 20:08:50 +0530

    pstar: Import config.fs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,6 +71,9 @@ TARGET_USES_GRALLOC4 := true
 TARGET_USES_HWC2 := true
 TARGET_USES_ION := true
 
+# Filesystem
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
+
 #HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
