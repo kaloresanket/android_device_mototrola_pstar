ec5b9faa6022d4107e9575c04fde72d5cc1ba0f9
kaloresanket <kaloresanket@gmail.com>
2023-09-21 18:25:41 +0530

    pstar: fix a2dp issues

--- device.mk
+++ device.mk
@@ -97,7 +97,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml
 
 # Boot control
