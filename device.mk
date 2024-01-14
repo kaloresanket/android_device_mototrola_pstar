f6129cecc843d25c5882b4f2d2753fb69811ec90
kaloresanket <kaloresanket@gmail.com>
2024-01-14 04:51:01 +0000

    pstar : Stop builiding unrequired packages

--- device.mk
+++ device.mk
@@ -100,12 +100,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti \
     android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service \
-    bootctrl.kona \
-    bootctrl.kona.recovery
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
+    android.hardware.boot@1.1-service
 
 # Camera
 PRODUCT_PACKAGES += \
